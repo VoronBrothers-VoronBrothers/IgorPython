cf66e08772d60c9ac48aabca44323a06eac88fb1
VoronBrothers <Raven12172@gmail.com>
2023-01-08 23:22:04 +0300

    1

---
+++ ClassIgor.py
@@ -0,0 +1,22 @@
+class Igor:
+    'The Igor class is created for teaching of junior students'
+    name = 'Igor'
+    age = 28
+    tall = 175
+
+    def __init__(self, x = 1, y = 1):
+        print(f"Constructor X and Y == {x}, {y}")
+        self.x = x
+        self.y = y
+
+    def __del__(self):
+        print(f'I am dead X and Y == {self.x}, {self.y}')
+
+    def vb(self):
+        if self.age == 55:
+            print('Secret age')
+        else:
+            return self.age
+
+    def vb2(self):
+        print('Function of class')
